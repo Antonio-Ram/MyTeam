aed92bf380fae1c018fc0fad08ef568d7249abe9
Antonio <tonito@umich.edu>
2021-12-11 22:06:25 -0500

    Employee push

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-commandLineArgs = process.argv;
-console.log(commandLineArgs);
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
+inquirer
+    .prompt([
+        {
+           type: "input",
+           name: "Name",
+           message: "Employee name?"
+       },
+       {
+           type: "input",
+           name: "ID",
+           message: "Employee ID?"
+       },
+       {
+           type: "input",
+           name: "Email",
+           message: "Employee email?"
+       }
+    ])
+    .then((answers) => {
+        fs.writeFile('./index.html', generatePage(name), err => {
+            if (err) throw err;
+        
+            console.log('HTML complete')
+        });
+    })
